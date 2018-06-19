22435a0f21806d003ffec21d4d648d5120d6c5dc
Jessamyn McTwigan <jessamyn.mctwigan@gmail.com>
2018-06-18 22:58:27 -0500

    inception dream

--- app.js
+++ app.js
@@ -1,68 +1,135 @@
 console.log("homework js datatypes");
-//
+
 // Answer the Following
 // For each of the following, write which datatypes you would use to represent the data, and then give a small example of the data structure:
 //
 // A light switch that can be either on or off.
+//
+// booleans true or false "light switch on = true"
+//
 // A user's email address.
+//
+// an array, a string method with quotes, ('me@gmail,com')
+//
 // A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+//
+// const Object => {
+//   hull: "metal",
+//   laser blasters: "5.0",
+//   tractor beam: "set to kill",
+//   warp drive: "20 parsects"
+// }
+//
 // A list of student names from our class.
+//
+// const Array = ['jessa', 'ryan', 'chris', 'timothy', 'daniel', 'ect...']
+//
 // A list of student names from our class, each with a location.
+//
+// // Array with properties and values / nested with objects
+//
+// const Array = [{
+//   name: 'jessa',
+//   city: 'st augustine',
+// };
+// {
+//   name: 'chris',
+//   city: 'denver',
+// }
+// {
+//   name: 'timothy',
+//   city: 'austin',
+// }]
+//
+//
 // A list of student names from our class, each with a location and each with a list of favorite tv shows.
 
+// Array with properties and values / nested also... what is the difference? are they each objects?
 
-
-
+// name: 'jessa',
+// city: 'st augustine',
+// tv shows: ['parks and recreation', 'homeland']
+// };
+// {
+// name: 'chris',
+// city: 'denver',
+// tv shows: ['sense8', 'boss baby']
+// }
+// {
+// name: 'timothy',
+// city: 'austin',
+// tv shows: ['star trek', 'wings']
+// }]
+//
 // Take it Easy
 // Make an array that holds all of the colors of the rainbow.
 // Write code that will access "blue" from the rainbow array.
 // Make an object that is called your name and holds the information about your favorite food, a hobby, the name of the town that you live in currently, and your favorite datatype.
 // Write code that will access your hobby from the object that you just created.
 
-
-
-
-const crazyObject = {
-  taco: [{meat: 'steak',
-         cheese: ['panela', 'queso', 'chihuahua']},
-         {meat: 'chicken',
-          salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
-        ],
-  larry: {
-    nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
-    quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
-    characters: [{
-      name: "Jeff",
-      occupation: "manager"
-    },
-    {
-      name: "funkhauser",
-      occupation: "tv dude"
-    },
-    {
-      name: "susie",
-      occupation: "jeffs wife",
-      favourtieHobby: "Swearing at Larry and Jeff"
-    },
-    ]
-  }
-}
-Use crazy Object to log the following.
-
-"omg my mouth is burning"
-
-"Pretty pretty prettayyyyy good"
-
-"Swearing at Larry and Jeff"
-
-"Chicken Teriyaki Boyyyyyy"
-
-The object the contains the name funkhauser
-
-
-
-Object-ception
-With the following object:
+// const spectrum =['red', 'orange', 'yellow', 'green', 'blue', 'purple' ];
+//
+// console.log(spectrum[4]);
+//
+// const jessamyn = {
+//   favoriteFood: 'Ramen',
+//   hobby: 'eating',
+//   city: 'Austin',
+//   favoriteDateType: 'booleans'
+// }
+// console.log(jessamyn.hobby);
+//
+//
+// const crazyObject = {
+//   taco: [{meat: 'steak',
+//          cheese: ['panela', 'queso', 'chihuahua']},
+//          {meat: 'chicken',
+//           salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
+//         ],
+//   larry: {
+//     nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+//     quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+//     characters: [{
+//       name: "Jeff",
+//       occupation: "manager"
+//     },
+//     {
+//       name: "funkhauser",
+//       occupation: "tv dude"
+//     },
+//     {
+//       name: "susie",
+//       occupation: "jeffs wife",
+//       favourtieHobby: "Swearing at Larry and Jeff"
+//     },
+//     ]
+//   }
+// }
+// // Use crazy Object to log the following.
+// //
+// // "omg my mouth is burning"
+// //
+// console.log(crazyObject.taco[1].salsa[5]);
+//
+//
+// // "Pretty pretty prettayyyyy good"
+// //
+// console.log(crazyObject.larry.quotes[0]);
+//
+// // "Swearing at Larry and Jeff"
+// //
+// console.log(crazyObject.larry.characters[2].favourtieHobby);
+//
+// // "Chicken Teriyaki Boyyyyyy"
+// //
+// console.log(crazyObject.larry.nicknames[1]);
+//
+// // The object the contains the name funkhauser
+// //
+// console.log(crazyObject.larry.characters[1]);
+//
+// Object-ception
+// With the following object:
 
 const inception = {
    reality: {
@@ -81,14 +148,32 @@ const inception = {
        }
    }
 }
-Change the value of limbo to null.
-
-
-
-
-
-Bond Films
-Array of objects:
+// Change the value of limbo to null.
+//
+inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = null;
+console.log(inception);
+output looks really cool in console...
+//{reality:
+  {…}}reality:
+    dreamLayer1:
+      dreamLayer2:
+        dreamLayer3:
+          dreamLayer4:
+            dreamLayer5:
+              dreamLayer6:
+                limbo: null__
+                proto__: Object__
+              proto__: Object_
+            proto__: Object__
+          proto__: Object__
+        proto__: Object__
+      proto__: Object__
+    proto__: Object__
+  proto__: Object
+//
+//
+// Bond Films
+// Array of objects:
 const bondFilms = [
   { "title" : "Skyfall", "year" : 2012, "actor" : "Daniel Craig", "gross" : "$1,108,561,008" },
   { "title" : "Thunderball", "year" : 1965, "actor" : "Sean Connery", "gross" : "$1,014,941,117" },
@@ -114,14 +199,14 @@ const bondFilms = [
   { "title" : "A View to a Kill", "year" : 1985, "actor" : "Roger Moore", "gross" : "$321,172,633" },
   { "title" : "License to Kill", "year" : 1989, "actor" : "Timothy Dalton", "gross" : "$285,157,191" }
 ];
-Yikes. Well, copy the bondFilms array of objects above into your js file. Use for loops and conditionals and methods in order to complete the below:
-
-Create a new array called bondTitles with only the titles of the Bond films, and console.log the new array.
-
-
-
-Create a new array oddBonds, of only the Bond films released on odd-numbered years.
-
-
-
-Determine the total cumulative gross of the Bond franchise, and console.log the result. hint To make the grosses into usable numbers, look into the .replace Javascript method (there are many ways to do this, however). Look into parseInt also.
+// Yikes. Well, copy the bondFilms array of objects above into your js file. Use for loops and conditionals and methods in order to complete the below:
+//
+// Create a new array called bondTitles with only the titles of the Bond films, and console.log the new array.
+//
+//
+//
+// Create a new array oddBonds, of only the Bond films released on odd-numbered years.
+//
+//
+//
+// Determine the total cumulative gross of the Bond franchise, and console.log the result. hint To make the grosses into usable numbers, look into the .replace Javascript method (there are many ways to do this, however). Look into parseInt also.
